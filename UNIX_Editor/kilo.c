5834b0affcf2b28b67ffc0b2ef61b0fe3fd6364c
K-Ravi-Chandra <cs19b020@iittp.ac.in>
2022-03-24 23:50:17 +0530

    added editor

--- UNIX_Editor/kilo.c
+++ UNIX_Editor/kilo.c
@@ -1191,7 +1191,7 @@ int main(int argc, char *argv[]) {
   }
 
   editorSetStatusMessage(
-    "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
+    "Press Enter to edit");
 
   while (1) {
     editorRefreshScreen();
